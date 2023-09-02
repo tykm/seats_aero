ecc2bf856618b3cecfde367e57ff0fc1ff250eb2
tykm <tylerkim01@gmail.com>
2023-09-02 17:40:14 -0400

    Added ability to pull from multiple sources

--- seats_aero.py
+++ seats_aero.py
@@ -2,14 +2,22 @@ import requests
 import json
 
 url = "https://seats.aero/api/availability"
-params = {"source": "lifemiles"}
-response = requests.get(url, params)
+sources = ["lifemiles", "delta", "virginatlantic"]
+all_data = []
 
-if response.status_code == 200:
-    data = response.json()
-    formatted_data = json.dumps(data, indent=4)  # Indent for better readability
-    with open("output.json", "w") as outfile:
-        outfile.write(formatted_data)
-    print("Data saved to 'output.json'")
-else:
-    print(f"Request failed with status code: {response.status_code}")
+for source in sources:
+    params = {"source": source}
+    response = requests.get(url, params)
+    if response.status_code == 200:
+        data = response.json()
+        formatted_data = json.dumps(data, indent=4)  # Indent for better readability
+        all_data.append(data)
+        print(f"Data received from {source}")
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+
+output_file = "output.json"
+with open(output_file, "w") as outfile:
+    json.dump(all_data, outfile, indent=4)
+
+print(f"All data saved to '{output_file}'")
